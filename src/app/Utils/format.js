91bb0214758340cb523d4fbbe8413362ba57e179
Tien Thanh <tienmt@imt-soft.com>
2024-02-09 21:44:12 +0700

    update code

--- src/app/Utils/format.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // Chuyển số sang định dạng tiền VNĐ
-  formatToVND: (amount) => {
-    return new Intl.NumberFormat("vi-VN", {
-      style: "currency",
-      currency: "VND",
-    }).format(amount);
-  },
-};
